8ffcedae8ff322a79155a5df73d80fa288267d51
Bryony Louise <bryonylouise2@hotmail.com>
2024-10-17 13:11:05 -0500

    Create 2.5.Combining_SPI_datasets.py

---
+++ 2.5.Combining_SPI_datasets.py
@@ -0,0 +1,58 @@
+#########################################################################################
+## Script to combine the multiple SPI.nc files for each region into one 
+## Bryony Louise
+## Last Edited: Thursday October 17th 2024 
+#########################################################################################
+#Import Required Modules
+#########################################################################################
+import xesmf as xe
+import numpy as np
+import xarray as xr
+import dask
+from tqdm import tqdm
+import time
+from datetime import datetime, timedelta, date
+from netCDF4 import Dataset, num2date, MFDataset
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import spei as si
+import pandas as pd
+import scipy.stats as scs
+import os
+import gzip
+
+#########################################################################################
+#Import Functions
+#########################################################################################
+import functions
+
+#########################################################################################
+#Regions For Analysis
+#########################################################################################
+#Choose Region
+Regions = {"WC", "MWN", "MWC", "MWS", "NGP", "SGP", "NGL", "SGL", "SNE", "WSE", "ESE"}
+              
+#########################################################################################
+#Import Data
+#########################################################################################
+#########################################################################################
+#Import Data - load in all files
+#########################################################################################
+dirname= '/scratch/bpuxley/SPI_30day'
+
+pathfiles = []
+for i in Regions:
+        filename = 'SPI30_%s.nc'%(i)
+        pathfile = os.path.join(dirname, filename)
+        pathfiles.append(pathfile)
+
+
+datasets = [xr.open_dataset(f) for f in pathfiles]
+print('Read in Data')
+
+combined = xr.combine_by_coords(datasets)
+print('Combined Datafiles')
+
+combined.to_netcdf('scratch/bpuxley/SPI_30day/spi_CONUS.nc')
+print('Saved file to netcdf')
