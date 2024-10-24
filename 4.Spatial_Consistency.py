7f10320080cb5e9cfe420699287b6d2db1cd2d2b
Bryony Louise <bryonylouise2@hotmail.com>
2024-10-24 10:39:16 -0500

    Update 4.Spatial_Consistency.py

--- 4.Spatial_Consistency.py
+++ 4.Spatial_Consistency.py
@@ -1,7 +1,7 @@
 #########################################################################################
 ## Consider the spatial continuity of grid points experiencing a whiplash event to determine events
 ## Bryony Louise
-## Last Edited: Wednesday October 16th 2024 
+## Last Edited: Wednesday October 23rd 2024 
 #########################################################################################
 #Import Required Modules
 #########################################################################################
@@ -37,22 +37,25 @@ grid_lats = np.arange(17, 58.01, 0.1)
 #########################################################################################
 
 #########################################################################################
-#Import Data
+#Import Data - load in all files
 #########################################################################################
-filename = 'whiplashes.nc'
-dirname= '/data2/bpuxley/Whiplash'
+dirname = '/scratch/bpuxley/Whiplash/'
+files = ['whiplashes_CONUS_1915_1967.nc', 'whiplashes_CONUS_1968_2020.nc']
+pathfile1 = os.path.join(dirname, files[0])
+pathfile2 = os.path.join(dirname, files[0])
 
-pathfile = os.path.join(dirname, filename)
+dataset1 = xr.open_dataset(pathfile1)
+dataset2 = xr.open_dataset(pathfile2)
 
-df_whiplash = xr.open_dataset(pathfile)
+dataset = dataset1
+
+lon, lat = np.meshgrid(dataset.lon.values, dataset.lat.values)
+m,o,p = dataset.DP_xarray.shape
+
 print('Read in Data')
-
-m,o,p = len(df_whiplash.time), len(df_whiplash.lat), len(df_whiplash.lon) #length of data #mo,o,p: time, lat, lon 
-lon, lat = np.meshgrid(df_whiplash.lon.values, df_whiplash.lat.values) #create a meshgrid of lat,lon values
-
-print('Time: '+ str(m))
-print('Lat: '+ str(o))
-print('Lon: '+ str(p))
+print('Time: ' + str(m))
+print('Lat: ' + str(o))
+print('Lon: ' + str(p))
 
 #########################################################################################
 #Determine Spatial Consistency
@@ -61,37 +64,37 @@ density_DP = np.zeros((m,o,p))
 density_PD = np.zeros((m,o,p))
 
 for i in tqdm(range(29, m-30)): #loop through the timeseries from value 29 (first 30 values will be all nans) up until the last 30 days
-	DP_field = df_whiplash.DP_whiplashes[i,:,:].values
-	if np.any(DP_field): #only run KDE code if at least one of the values is 1
-		density_DP[i, :, :] = functions.kde(orig_lon=df_whiplash.lon.values,
-										orig_lat=df_whiplash.lat.values,
-               							grid_lon=df_whiplash.lon.values,
-                						grid_lat=df_whiplash.lat.values,
-                						extreme=DP_field,
-                						bandwidth=0.025,
-           								)
+        DP_field = dataset.DP_xarray[i,:,:].values
+        if np.any(DP_field): #only run KDE code if at least one of the values is 1
+                density_DP[i, :, :] = functions.kde(orig_lon=dataset.lon.values,
+                                                                orig_lat=dataset.lat.values,
+                                                                grid_lon=dataset.lon.values,
+                                                                grid_lat=dataset.lat.values,
+                                                                extreme=DP_field,
+                                                                bandwidth=0.025,
+                                                                        )
 
 #########################################################################################
 #Save out the density file 
 #########################################################################################
 time = pd.date_range(start='1915-01-01', end='2020-12-31', freq='D')
-lats = df_whiplash.lat
-lons = df_whiplash.lon
-attrs = df_whiplash.attrs
+lats = dataset.lat
+lons = dataset.lon
+attrs = dataset.attrs
 
 dimensions=['time','lat','lon']
 coords = {
-			'time': time,
-			'lat': lats,
-			'lon': lons
-			}
+                        'time': time,
+                        'lat': lats,
+                        'lon': lons
+                        }
 #attributes = {'sources': 'Livneh et al., 2013 & PRISM, 2004', 'references': 'http://www.esrl.noaa.gov/psd/data/gridded/data.livneh.html','}
 
-DP_xarray = xr.DataArray(density_DP, coords, dims=dimensions, name='Drought to Pluvial')
-PD_xarray = xr.DataArray(density_PD, coords, dims=dimensions, name='Pluvial to Drought')
-density_dataset = xr.Dataset({"DP_density":DP_xarray, "PD_density":PD_xarray})
+DP_density = xr.DataArray(density_DP, coords, dims=dimensions, name='Drought to Pluvial')
+PD_density = xr.DataArray(density_PD, coords, dims=dimensions, name='Pluvial to Drought')
+density_dataset = xr.Dataset({"DP_density":DP_density, "PD_density":PD_density})
 
-density_dataset.to_netcdf('/home/bpuxley/Whiplash/density.nc')
+density_dataset.to_netcdf('/scratch/bpuxley/Whiplasih/density_1915_1967.nc')
 print('density file saved')
 
 #########################################################################################
