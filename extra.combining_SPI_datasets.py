ff06c567b4159866d416629e890c9c2675497dae
Bryony Louise <bryonylouise2@hotmail.com>
2024-11-15 15:12:32 -0600

    Update extra.combining_SPI_datasets.py

--- extra.combining_SPI_datasets.py
+++ extra.combining_SPI_datasets.py
@@ -34,7 +34,7 @@ Regions = {"WCN", "WCS", "MWN", "MWC", "MWS", "NGP", "SGP", "NGL", "SGL", "SNE",
 #########################################################################################
 #Import Data - load in all files
 #########################################################################################
-dirname= '/scratch/bpuxley/SPI_30day'
+dirname= '/data2/bpuxley/SPI_30day'
 
 pathfiles = []
 for i in Regions:
@@ -47,17 +47,17 @@ datasets = [xr.open_dataset(f) for f in pathfiles]
 print('Read in Data')
 
 #Split spi data in 10 year time periods
-time_1915_1924 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1925_1934 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1935_1944 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1945_1954 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1955_1964 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1965_1974 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1975_1984 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1985_1994 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_1995_2004 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_2005_2014 = [ds.isel(time=slice(0,19358)) for ds in datasets]
-time_2015_2020 = [ds.isel(time=slice(0,19358)) for ds in datasets]
+time_1915_1924 = [ds.sel(time=slice("1915-01-01","1924-12-31")) for ds in datasets]
+time_1925_1934 = [ds.sel(time=slice("1925-01-01","1934-12-31")) for ds in datasets]
+time_1935_1944 = [ds.sel(time=slice("1935-01-01","1944-12-31")) for ds in datasets]
+time_1945_1954 = [ds.sel(time=slice("1945-01-01","1954-12-31")) for ds in datasets]
+time_1955_1964 = [ds.sel(time=slice("1955-01-01","1964-12-31")) for ds in datasets]
+time_1965_1974 = [ds.sel(time=slice("1965-01-01","1974-12-31")) for ds in datasets]
+time_1975_1984 = [ds.sel(time=slice("1975-01-01","1984-12-31")) for ds in datasets]
+time_1985_1994 = [ds.sel(time=slice("1985-01-01","1994-12-31")) for ds in datasets]
+time_1995_2004 = [ds.sel(time=slice("1995-01-01","2004-12-31")) for ds in datasets]
+time_2005_2014 = [ds.sel(time=slice("2005-01-01","2014-12-31")) for ds in datasets]
+time_2015_2020 = [ds.sel(time=slice("2015-01-01","2020-12-31")) for ds in datasets]
 
 combined = xr.combine_by_coords(datasets)
 print('Combined Datafiles')
