a4215e21e2de78c87725a812ed415882cd7d124b
Bryony Louise <bryonylouise2@hotmail.com>
2024-11-15 15:08:00 -0600

    Update extra.event_examination.py

--- extra.event_examination.py
+++ extra.event_examination.py
@@ -26,16 +26,12 @@ import os
 #Dates for Examination
 #########################################################################################
 #Drought-to-Pluvial
-DP_date = date(1938, 12, 18)
-DP_index_spi = (DP_date - date(1915, 1, 1)).days
-DP_index_whiplash = (DP_date - date(1915, 1, 1)).days
-DP_index_density = (DP_date - date(1935, 1, 1)).days
+DP_date_drought = "1938-12-18"
+DP_date_pluvial = str(date(1938, 12, 18) + timedelta(days=30))
 
 #Pluvial-to-Drought
-PD_date = date(1991, 9, 24)
-PD_index_spi = (PD_date - date(1915, 1, 1)).days
-PD_index_whiplash = (PD_date - date(1915, 1, 1)).days
-PD_index_density = (PD_date - date(1988, 1, 1)).days
+PD_date_pluvial = "1991-09-24"
+PD_date_drought = str(date(1991, 9, 24) + timedelta(days=30))
 
 #########################################################################################
 #Import Data
@@ -63,16 +59,16 @@ print('Read in Data')
 #Select only dates required for examination
 #########################################################################################
 #Drought-to-Pluvial
-df_spi_dp_drought = df_spi.spi_30day.isel(time=DP_index_spi)
-df_spi_dp_pluvial = df_spi.spi_30day.isel(time=DP_index_spi+31)
-df_whiplash_dp = df_whiplash.DP_whiplashes.isel(time=DP_index_whiplash)
-df_density_dp = df_density_DP.DP_density.isel(time=DP_index_density)
+df_spi_dp_drought = df_spi.spi_30day.sel(time=DP_date_drought)
+df_spi_dp_pluvial = df_spi.spi_30day.sel(time=DP_date_pluvial)
+df_whiplash_dp = df_whiplash.DP_whiplashes.sel(time=DP_date_drought)
+df_density_dp = df_density_DP.DP_density.sel(time=DP_date_drought)
 
 #Pluvial-to-Drought
-df_spi_pd_pluvial = df_spi.spi_30day.isel(time=PD_index_spi)
-df_spi_pd_drought = df_spi.spi_30day.isel(time=PD_index_spi+31)
-df_whiplash_pd = df_whiplash.PD_whiplashes.isel(time=PD_index_whiplash)
-df_density_pd = df_density_PD.PD_density.isel(time=PD_index_density)
+df_spi_pd_pluvial = df_spi.spi_30day.sel(time=PD_date_pluvial)
+df_spi_pd_drought = df_spi.spi_30day.sel(time=PD_date_drought)
+df_whiplash_pd = df_whiplash.PD_whiplashes.sel(time=PD_date_pluvial)
+df_density_pd = df_density_PD.PD_density.sel(time=PD_date_pluvial)
 
 #########################################################################################
 #Filter date for plotting
