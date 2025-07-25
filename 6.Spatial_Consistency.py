dc3a948be02dd7514d1b01c52600bcf0031e6196
Bryony Louise <bryonylouise2@hotmail.com>
2025-07-25 16:27:52 -0500

    Update 6.Spatial_Consistency.py

--- 6.Spatial_Consistency.py
+++ 6.Spatial_Consistency.py
@@ -1,7 +1,10 @@
 #########################################################################################
-## Consider the spatial continuity of grid points experiencing a whiplash event to determine events
+## Using Kernel Density Estimation; consider the spatial continuity of grid points 
+## experiencing a whiplash event to determine events.
 ## Bryony Louise
-## Last Edited: Friday February 28th 2025 
+## Last Edited: Friday, July 25th, 2025 
+## Input: Decadal whiplash occurrence files.
+## Output: 
 #########################################################################################
 #Import Required Modules
 #########################################################################################
@@ -21,7 +24,6 @@ import pandas as pd
 import scipy.stats as scs
 import os
 from sklearn.neighbors import KernelDensity
-
 from matplotlib.colors import ListedColormap
 
 #########################################################################################
@@ -32,85 +34,93 @@ import functions
 #########################################################################################
 #Import Data - load in all files
 #########################################################################################
-years = ['1915_1924', '1925_1934', '1935_1944', '1945_1954', '1955_1964', '1965_1974', 
-			'1975_1984', '1985_1994', '1995_2004', '2005_2014', '2015_2020']
+time_periods = {'1915_1924', '1925_1934', '1935_1944', '1945_1954', '1955_1964', '1965_1974', 
+			'1975_1984', '1985_1994', '1995_2004', '2005_2014', '2015_2020'}
 
-#Whiplash Points from 4.Whiplash_Identification
+#Whiplash Points from 5.Convert_regional_whiplash_data_into_decadal_periods.py
 dirname= '/scratch/bpuxley/Whiplash'
 
 pathfiles = []
 
-for i in years:
-	filename = 'whiplashes_%s.nc'%(i)
+for period in sorted(time_periods):
+	print(period)
+
+        ################################
+        # Read in data
+        ################################
+	filename = 'whiplashes_%s.nc'%(period)
 	pathfile = os.path.join(dirname, filename)
-	pathfiles.append(pathfile)
+        df = xr.open_dataset(pathfile)
+        print('Read in Data')
 
-whiplashes = xr.open_mfdataset(pathfiles, combine='by_coords')
+        m,o,p = len(df.time), len(df.lat), len(df.lon) #length of data #mo,o,p: time, lat, lon 
+        lon, lat = np.meshgrid(df.lon.values, df.lat.values) #create a meshgrid of lat,lon values
 
-#Split into managable time chunks for data processing
-data_slice = years[0] #choose which years to look at
-start_year = years[0][0:4]
-end_year = years[0][5:9]
-dataset_DP = whiplashes.DP_whiplashes.sel(time=slice(start_year +'-01-01', end_year +'-12-31'))
-dataset_PD = whiplashes.PD_whiplashes.sel(time=slice(start_year +'-01-01', end_year +'-12-31'))
+        print('Time: '+ str(m))
+        print('Lat: '+ str(o))
+        print('Lon: '+ str(p))
 
-lon, lat = np.meshgrid(dataset_DP.lon.values, dataset_DP.lat.values)
-m,o,p = dataset_DP.shape
+        DP_dataset = df.DP_whiplashes
+        PD_dataset = df.PD_whiplashes
+        
+	################################
+        # Determine Spatial Consistency
+        ################################
+        density_DP = np.zeros((m,o,p))
+        density_PD = np.zeros((m,o,p))
 
-print('Read in Data')
-print('Time: ' + str(m))
-print('Lat: ' + str(o))
-print('Lon: ' + str(p))
-
-#########################################################################################
-#Determine Spatial Consistency
-#########################################################################################
-density_DP = np.zeros((m,o,p))
-density_PD = np.zeros((m,o,p))
-
-for i in tqdm(range(0, m)): #loop through the timeseries: when looking at the beginning of the time series turn 0 to 29 and when looking at the end of the series turn m to m-30 (first and last 30 values will be all nans)
-        DP_field = dataset_DP[i,:,:].values
-        if np.any(~np.isnan(DP_field)): #only run KDE code if at least one of the values is 1
-                density_DP[i, :, :] = functions.kde(orig_lon=dataset_DP.lon.values,
-                                                                orig_lat=dataset_DP.lat.values,
-                                                                grid_lon=dataset_DP.lon.values,
-                                                                grid_lat=dataset_DP.lat.values,
+        start=time.time()
+        print('Drought-to-Pluvial: Started')
+        for i in tqdm(range(0, m)): #loop through the timeseries: when looking at the beginning of the time series, turn 0 to 2,9 and when looking at the end of the series, turn m to m-30 (first and last 30 values will be all nans)
+                DP_field = DP_dataset[i,:,:].values
+                if np.any(~np.isnan(DP_field)): #only run KDE code if at least one of the values is 1
+                        density_DP[i, :, :] = functions.kde(orig_lon=DP_dataset.lon.values,
+                                                                orig_lat=DP_dataset.lat.values,
+                                                                grid_lon=DP_dataset.lon.values,
+                                                                grid_lat=DP_dataset.lat.values,
                                                                 extreme=DP_field,
                                                                 bandwidth=0.025,
-                                                                        )
+                                                                                )
+        print('Drought-to-Pluvial: Ended')
+        end=time.time()
+        time_taken = (end-start)/3600
+        print('Time Taken: '+str(time_taken))
 
-for i in tqdm(range(0, m)): #loop through the timeseries: when looking at the beginning of the time series turn 0 to 29 and when looking at the end of the series turn m to m-30 (first and last 30 values will be all nans)
-        PD_field = dataset_PD[i,:,:].values
-        if np.any(~np.isnan(PD_field)): #only run KDE code if at least one of the values is 1
-                density_PD[i, :, :] = functions.kde(orig_lon=dataset_PD.lon.values,
-                                                                orig_lat=dataset_PD.lat.values,
-                                                                grid_lon=dataset_PD.lon.values,
-                                                                grid_lat=dataset_PD.lat.values,
+        start=time.time()
+        print('Pluvial-to-Drought: Started')
+        for i in tqdm(range(0, m)): #loop through the timeseries: when looking at the beginning of the time series turn 0 to 29 and when looking at the end of the series turn m to m-30 (first and last 30 values will be all nans)
+                PD_field = PD_dataset[i,:,:].values
+                if np.any(~np.isnan(PD_field)): #only run KDE code if at least one of the values is 1
+                        density_PD[i, :, :] = functions.kde(orig_lon=PD_dataset.lon.values,
+                                                                orig_lat=PD_dataset.lat.values,
+                                                                grid_lon=PD_dataset.lon.values,
+                                                                grid_lat=PD_dataset.lat.values,
                                                                 extreme=PD_field,
                                                                 bandwidth=0.025,
-                                                                        )
+                                                                                )
+        print('Pluvial-to-Drought: Ended')
+        end=time.time()
+        time_taken=(end-start)/3600
+        print('Time Taken: '+str(time_taken))                                                               
 
+        ################################
+        #Save out the density file 
+        ##########i######################
+        time_frame = DP_dataset.time
+        lats = DP_dataset.lat
+        lons = DP_dataset.lon
 
-#########################################################################################
-#Save out the density file 
-#########################################################################################
-time = dataset_DP.time
-lats = dataset_DP.lat
-lons = dataset_DP.lon
-attrs = dataset_DP.attrs
-
-dimensions=['time','lat','lon']
-coords = {
-                        'time': time,
-                        'lat': lats,
-                        'lon': lons
+        dimensions=['time', 'lat', 'lon']
+        coords = {
+                'time': time_frame,
+                'lat': lats,
+                'lon': lons
                         }
-#attributes = {'sources': 'Livneh et al., 2013 & PRISM, 2004', 'references': 'http://www.esrl.noaa.gov/psd/data/gridded/data.livneh.html','}
 
-DP_density = xr.DataArray(density_DP, coords, dims=dimensions, name='Drought to Pluvial')
-PD_density = xr.DataArray(density_PD, coords, dims=dimensions, name='Pluvial to Drought')
-density_dataset = xr.Dataset({"DP_density":DP_density, "PD_density":PD_density})
+       	DP_density = xr.DataArray(density_DP, coords, dims=dimensions, name='Drought-to-Pluvial')
+        PD_density = xr.DataArray(density_PD, coords, dims=dimensions, name='Pluvial-to-Drought')
+        density_dataset = xr.Dataset({"DP_density":DP_density, "PD_density":PD_density})
 
-#Remember to change filename to whatever years have been calculated
-density_dataset.to_netcdf('/scratch/bpuxley/Density/density_%s.nc'%(data_slice))
-print('density file saved')
+        density_dataset.to_netcdf('/scratch/bpuxley/density/density_%s.nc'%(period))
+        print('density file saved')
+        print('Period: '+period+' completed')
