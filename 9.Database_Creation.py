0f805fe2e8202ef1bc49ec4833989940c7ead569
Bryony Louise <bryonylouise2@hotmail.com>
2025-07-28 14:49:32 -0500

    Update 9.Database_Creation.py

--- 9.Database_Creation.py
+++ 9.Database_Creation.py
@@ -1,14 +1,15 @@
 ########################################################################################
 ## This script creates the databases of all of the precipitation whiplash events, subsetting 
 ## by areal threshold (can be set to zero to keep all events) and also calculates relevant 
-## statistics such as: the area averaged SPI during the 30-day drought period, the area-averaged
+## statistics such as: the area-averaged SPI during the 30-day drought period, the area-averaged
 ## SPI during the 30-day pluvial period, the area-averaged SPI change, the grid point magnitude 
 ## maximum SPI during the 30-day drought and pluvial periods, respectively, and the grid point
 ## maximum SPI change.
 ## Bryony Louise Puxley
 ## Last Edited: Monday, July 28th, 2025 
-## Input:
-## Output: 
+## Input: Decadal SPI files, decadal whiplash files, decadal density/KDE files, and decadal 
+## potential events CSV files.
+## Output: Two CSV files of drought-to-pluvial and pluvial-to-drought events.
 #########################################################################################
 #Import Required Modules
 #########################################################################################
@@ -41,7 +42,7 @@ import functions
 AREA = 175000
 
 #########################################################################################
-#Import Data - load previously made spi, whiplash identification and potential events files
+#Import Data - load previously made SPI, whiplash identification, and potential events files
 #########################################################################################
 years = ['1915_1924', '1925_1934', '1935_1944', '1945_1954', '1955_1964', '1965_1974', 
 			'1975_1984', '1985_1994', '1995_2004', '2005_2014', '2015_2020']
@@ -118,14 +119,13 @@ polygons_PD = [shapely.wkt.loads(i) for i in potential_events_PD.geometry] #conv
 #Notes for Dates
 #########################################################################################
 # consider a drought to pluvial whiplash where Jan 1 to Jan 30 is in drought and Jan 31 to March 1 is pluvial
-# SPI is saved so that January 30th refers to the 30 day SPI between Jan 1st and Jan 30th etc.
+# SPI is saved so that January 30th refers to the 30-day SPI between January 1st and January 30th, etc.
 # drought spi: Jan 30
 # pluvial spi: Mar 1
 # whiplash and density data are saved as the date the change occurred
 # here whiplash/density date: Jan 30
-# potential events are saved so drought date is the start of the drought month and pluvial is the start of the pluvial month
-# here drought date would be Jan 1 and pluvial date would be Jan 31
-
+# potential events are saved, so drought date is the start of the drought month, and pluvial is the start of the pluvial month
+# here, the drought date would be Jan 1, and the pluvial date would be Jan 31
 
 #########################################################################################
 #Calculate Relevant Statistics
@@ -140,6 +140,7 @@ spi_drought_max_DP = np.zeros_like(spi_drought_avg_DP)
 spi_pluvial_max_DP = np.zeros_like(spi_drought_avg_DP)
 spi_change_max_DP = np.zeros_like(spi_drought_avg_DP)
 
+print('Drought-to-Pluvial loop: started')
 for i,(event_date,poly) in enumerate(zip(potential_events_DP.Drought_Date, polygons_DP)):
 	event_date = event_date
 	drought_date = potential_events_DP.Whiplash_Date[i]
@@ -156,16 +157,18 @@ for i,(event_date,poly) in enumerate(zip(potential_events_DP.Drought_Date, polyg
 	spi_drought_max_DP[i] = stats['drought_spi_max']
 	spi_pluvial_max_DP[i] = stats['pluvial_spi_max']
 	spi_change_max_DP[i] = stats['spi_change_max']
-		
+print('Drought-to-Pluvial loop: ended')
+
 potential_events_DP['Avg_SPI_Drought'] = spi_drought_avg_DP
 potential_events_DP['Avg_SPI_Pluvial'] = spi_pluvial_avg_DP
 potential_events_DP['Avg_SPI_Change'] = spi_change_avg_DP
 potential_events_DP['MaxMag_SPI_Drought'] = spi_drought_max_DP
 potential_events_DP['MaxMag_SPI_Pluvial'] = spi_pluvial_max_DP
 potential_events_DP['Max_SPI_Change'] = spi_change_max_DP
-	
+
+print('Drought-to-Pluvial: saving file')
 potential_events_DP.to_csv(f'/data2/bpuxley/Events/events_DP.csv', index=False)
-print('saved Drought-to-Pluvial events')
+print('Drought-to-Pluvial: file saved')
 
 #Pluvial-to-Drought
 spi_drought_avg_PD = np.zeros((len(potential_events_PD.Pluvial_Date)))
@@ -176,7 +179,7 @@ spi_drought_max_PD = np.zeros_like(spi_drought_avg_PD)
 spi_pluvial_max_PD = np.zeros_like(spi_drought_avg_PD)
 spi_change_max_PD = np.zeros_like(spi_drought_avg_PD)
 
-
+print('Pluvial-to-Drought loop: started')
 for i,(event_date,poly) in enumerate(zip(potential_events_PD.Pluvial_Date, polygons_PD)):
 	event_date = event_date
 	pluvial_date = potential_events_PD.Whiplash_Date[i]
@@ -193,7 +196,8 @@ for i,(event_date,poly) in enumerate(zip(potential_events_PD.Pluvial_Date, polyg
 	spi_drought_max_PD[i] = stats['drought_spi_max']
 	spi_pluvial_max_PD[i] = stats['pluvial_spi_max']
 	spi_change_max_PD[i] = stats['spi_change_max']
-		
+print('Pluvial-to-Drought loop: ended')
+
 potential_events_PD['Avg_SPI_Drought'] = spi_drought_avg_PD
 potential_events_PD['Avg_SPI_Pluvial'] = spi_pluvial_avg_PD
 potential_events_PD['Avg_SPI_Change'] = spi_change_avg_PD
@@ -206,9 +210,10 @@ nan_rows = potential_events_PD.iloc[ind]
 print(nan_rows)
 potential_events_PD.drop(ind, inplace=True)
 potential_events_PD.reset_index(drop=True)
-	
+
+print('Pluvial-to-Drought: saving file')
 potential_events_PD.to_csv(f'/data2/bpuxley/Events/events_PD.csv', index=False)
-print('saved Pluvial-to-Drought events')
+print('Pluvial-to-Drought:file saved')
 
 
  
