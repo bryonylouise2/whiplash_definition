4ce0546be517aef1fa61a4edbc0fc4675c8d75ff
Bryony Louise <bryonylouise2@hotmail.com>
2025-08-08 14:01:35 -0500

    Update Ex.CaseStudyAnalysis_DP.py

--- Extra/Ex.CaseStudyAnalysis_DP.py
+++ Extra/Ex.CaseStudyAnalysis_DP.py
@@ -4,7 +4,7 @@
 ## Last Edited: Friday, August 8th, 2025
 ## Input: Decadal SPI, whiplash occurrences, normalized density, and independent 
 ## drought-to-pluvial events CSV. Choose event number or date.
-## Output: gifs of SPI during the drought period, SPI during the pluvial period, SPI change,
+## Output: GIFs of SPI during the drought period, SPI during the pluvial period, SPI change,
 ## and whiplash occurrences.
 #########################################################################################
 # Import Required Modules
