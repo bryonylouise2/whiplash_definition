5e4db3ea05c178765fc66aa51ec71545fd4e4bfd
Bryony Louise <bryonylouise2@hotmail.com>
2025-08-13 14:40:39 -0500

    Update 8.5.Drought_Pluvial_Make_Area_Plot.py

--- Droughts_Pluvials/8.5.Drought_Pluvial_Make_Area_Plot.py
+++ Droughts_Pluvials/8.5.Drought_Pluvial_Make_Area_Plot.py
@@ -1,7 +1,7 @@
 #########################################################################################
 ## Plot the distribution of all the areas 
 ## Bryony Louise Puxley
-## Last Edited: Monday, July 28th, 2025 
+## Last Edited: Wednesday, August 13th, 2025 
 ## Input: The CSV files of all potential events created in 8.Area_Calculation.py.
 ## Output: A PNG file of a 4-panel plot that includes a histogram of the frequency of 
 ## all areas, and a line plot of the percentiles relating to each area for both types
