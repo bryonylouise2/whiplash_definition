9694c6188a1103d5b53296c9d3cc0f2071b163e1
Bryony Louise <bryonylouise2@hotmail.com>
2025-08-13 12:43:31 -0500

    Update 6.Spatial_Consistency.py

--- 6.Spatial_Consistency.py
+++ 6.Spatial_Consistency.py
@@ -42,85 +42,83 @@ time_periods = {'1915_1924', '1925_1934', '1935_1944', '1945_1954', '1955_1964',
 dirname= '/scratch/bpuxley/Whiplash'
 
 for period in sorted(time_periods):
-		print(period)
+	print(period)
+	
+	################################
+	# Read in data
+	################################
+	filename = 'droughts_and_pluvials_%s.nc'%(period)	
+	pathfile = os.path.join(dirname, filename)
+	
+	df = xr.open_dataset(pathfile)
+	print('Read in Data')
+	
+	m,o,p = len(df.time), len(df.lat), len(df.lon) #length of data #mo,o,p: time, lat, lon 
+	lon, lat = np.meshgrid(df.lon.values, df.lat.values) #create a meshgrid of lat,lon values
+	
+	print('Time: '+ str(m))
+	print('Lat: '+ str(o))
+	print('Lon: '+ str(p))
+	drought_dataset = df.drought_events
+	pluvial_dataset = df.pluvial_events
+	
+	################################
+	# Determine Spatial Consistency
+	################################
+	density_drought = np.zeros((m,o,p))
+	density_pluvial = np.zeros((m,o,p))
+	
+	start = time.time()
+	print('Drought: Started')
+	for i in tqdm(range(29, m)): #loop through the timeseries: when looking at the beginning of the time series turn 0 to 29 and when looking at the end of the series turn m to m-30 (first and last 30 values will be all nans)
+		drought_field = drought_dataset[i,:,:].values
+		if np.any(drought_field): #only run KDE code if at least one of the values is 1
+			density_drought[i, :, :] = functions.kde(orig_lon=drought_dataset.lon.values,
+                                                     orig_lat=drought_dataset.lat.values,
+                                                     grid_lon=drought_dataset.lon.values,
+                                                     grid_lat=drought_dataset.lat.values,
+                                                     extreme=drought_field,
+                                                     bandwidth=0.025,
+                                                     )
+	
+	print('Drought: Ended')
+	end = time.time()
+	time_taken = (end-start)/3600
+	print('Time Taken: '+time_taken)
+	
+	print('Pluvial: Started')
+	for i in tqdm(range(0, m)): #loop through the timeseries: when looking at the beginning of the time series turn 0 to 29 and when looking at the end of the series turn m to m-30 (first and last 30 values will be all nans)
+		pluvial_field = dataset_pluvial[i,:,:].values
+		if np.any(pluvial_field): #only run KDE code if at least one of the values is 1
+			density_pluvial[i, :, :] = functions.kde(orig_lon=pluvial_dataset.lon.values,
+														orig_lat=pluvial_dataset.lat.values,
+														grid_lon=pluvial_dataset.lon.values,
+														grid_lat=pluvial_dataset.lat.values,
+														extreme=pluvial_field,
+														bandwidth=0.025,
+														)
+	
+	print('Pluvial: Ended')     
+	
+	################################
+	#Save out the density file 
+	################################
+	time_frame = drought_dataset.time
+	lats = drought_dataset.lat
+	lons = drought_dataset.lon
+	
+	dimensions=['time','lat','lon']
+	coords = {
+				'time': time_frame,
+				'lat': lats,
+				'lon': lons
+				}
 
-        ################################
-        # Read in data
-        ################################
-		filename = 'whiplashes_%s.nc'%(period)
-		pathfile = os.path.join(dirname, filename)
-        
-		df = xr.open_dataset(pathfile)
-        print('Read in Data')
+	drought_density = xr.DataArray(density_drought, coords, dims=dimensions, name='Drought')
+	pluvial_density = xr.DataArray(density_pluvial, coords, dims=dimensions, name='Pluvial')
+	density_dataset = xr.Dataset({"drought_density":drought_density, "pluvial_density":pluvial_density})
 
-        m,o,p = len(df.time), len(df.lat), len(df.lon) #length of data #mo,o,p: time, lat, lon 
-        lon, lat = np.meshgrid(df.lon.values, df.lat.values) #create a meshgrid of lat,lon values
-
-        print('Time: '+ str(m))
-        print('Lat: '+ str(o))
-        print('Lon: '+ str(p))
-
-        DP_dataset = df.DP_whiplashes
-        PD_dataset = df.PD_whiplashes
-        
-		################################
-        # Determine Spatial Consistency
-        ################################
-        density_DP = np.zeros((m,o,p))
-        density_PD = np.zeros((m,o,p))
-
-        start=time.time()
-        print('Drought-to-Pluvial: Started')
-        for i in tqdm(range(0, m)): #loop through the timeseries: when looking at the beginning of the time series, turn 0 to 29, and when looking at the end of the series, turn m to m-30 (first and last 30 values will be all nans)
-                DP_field = DP_dataset[i,:,:].values
-                if np.any(~np.isnan(DP_field)): #only run KDE code if at least one of the values is 1
-                        density_DP[i, :, :] = functions.kde(orig_lon=DP_dataset.lon.values,
-                                                                orig_lat=DP_dataset.lat.values,
-                                                                grid_lon=DP_dataset.lon.values,
-                                                                grid_lat=DP_dataset.lat.values,
-                                                                extreme=DP_field,
-                                                                bandwidth=0.025,
-                                                                                )
-        print('Drought-to-Pluvial: Ended')
-        end=time.time()
-        time_taken = (end-start)/3600
-        print('Time Taken: '+str(time_taken))
-
-        start=time.time()
-        print('Pluvial-to-Drought: Started')
-        for i in tqdm(range(0, m)): #loop through the timeseries: when looking at the beginning of the time series turn 0 to 29 and when looking at the end of the series turn m to m-30 (first and last 30 values will be all nans)
-                PD_field = PD_dataset[i,:,:].values
-                if np.any(~np.isnan(PD_field)): #only run KDE code if at least one of the values is 1
-                        density_PD[i, :, :] = functions.kde(orig_lon=PD_dataset.lon.values,
-                                                                orig_lat=PD_dataset.lat.values,
-                                                                grid_lon=PD_dataset.lon.values,
-                                                                grid_lat=PD_dataset.lat.values,
-                                                                extreme=PD_field,
-                                                                bandwidth=0.025,
-                                                                                )
-        print('Pluvial-to-Drought: Ended')
-        end=time.time()
-        time_taken=(end-start)/3600
-        print('Time Taken: '+str(time_taken))                                                               
-
-        ################################
-        #Save out the density file 
-        ##########i######################
-        time_frame = DP_dataset.time
-        lats = DP_dataset.lat
-        lons = DP_dataset.lon
-
-        dimensions=['time', 'lat', 'lon']
-        coords = {
-                'time': time_frame,
-                'lat': lats,
-                'lon': lons
-                        }
-
-       	DP_density = xr.DataArray(density_DP, coords, dims=dimensions, name='Drought-to-Pluvial')
-        PD_density = xr.DataArray(density_PD, coords, dims=dimensions, name='Pluvial-to-Drought')
-        density_dataset = xr.Dataset({"DP_density":DP_density, "PD_density":PD_density})
-
-        density_dataset.to_netcdf('/scratch/bpuxley/density/density_%s.nc'%(period))
-        print('density file saved')
-        print('Period: '+period+' completed')
+	density_dataset.to_netcdf('/data2/bpuxley/droughts_and_pluvials/density/density_%s.nc'%(period))
+	print('density file saved')
+	
+	print('Period: '+period+' completed')
