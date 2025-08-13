ebe3a863a557dd27ec8ef4af8eba2d3c1bd6f49d
Bryony Louise <bryonylouise2@hotmail.com>
2025-08-13 14:22:59 -0500

    Update 7.Drought_Pluvial_Density_Calculation.py

--- Droughts_Pluvials/7.Drought_Pluvial_Density_Calculation.py
+++ Droughts_Pluvials/7.Drought_Pluvial_Density_Calculation.py
@@ -1,22 +1,21 @@
 #########################################################################################
-#Import Required Modules
+## Script to combine the multiple density.nc files into one file and calculate the 99th 
+## percentile for polygons.
+## Bryony Louise Puxley
+## Last Edited: Monday, July 28th, 2025 
+## Input: decadal density files for drought and pluvial events.
+## Output: The 99th percentile of densities for drought and pluvial events to be used to 
+## draw the single contour for the polygon outlining the area defined as having a whiplash
+## event.
 #########################################################################################
-import xesmf as xe
+# Import Required Modules
+#########################################################################################
 import numpy as np
 import xarray as xr
-import dask
-from tqdm import tqdm
-import time
-from datetime import datetime, timedelta, date
-from netCDF4 import Dataset, num2date, MFDataset
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import spei as si
 import pandas as pd
+from netCDF4 import Dataset, num2date, MFDataset
 import scipy.stats as scs
 import os
-import gzip
 
 #########################################################################################
 #Time Periods
@@ -28,20 +27,18 @@ Years = {"1915_1924", "1925_1934", "1935_1944", "1945_1954", "1955_1964", "1965_
 #########################################################################################
 #Import Data - load in all files
 #########################################################################################
-#drought_dirname = '/scratch/bpuxley/droughts_and_pluvials/density/density_droughts_*.nc'
+drought_dirname = '/scratch/bpuxley/droughts_and_pluvials/density/density_droughts_*.nc'
 pluvial_dirname = '/scratch/bpuxley/droughts_and_pluvials/density/density_pluvials_*.nc'
 
-#drought_densities = xr.open_mfdataset(drought_dirname, combine='by_coords')
+drought_densities = xr.open_mfdataset(drought_dirname, combine='by_coords')
 pluvial_densities = xr.open_mfdataset(pluvial_dirname, combine='by_coords')
 
-#Save file out
-#all_densities.to_netcdf('/scratch/bpuxley/Density/density_1915_2020.nc')
-
 #Split into DP and PD
-#drought_densities = drought_densities.drought_density
+drought_densities = drought_densities.drought_density
 pluvial_densities = pluvial_densities.pluvial_density
 
 #Calculate the 99th percentile of density files to draw the polygon
-#drought_perc = np.percentile(drought_densities, q=99)
+drought_perc = np.percentile(drought_densities, q=99)
 pluvial_perc = np.percentile(pluvial_densities, q=99)
-print(f'the 99th percentile for Drought Events is 0.8921, for Pluvial Events is {pluvial_perc}, and they were calculated at {datetime.now()}')
+
+print(f'the 99th percentile for Drought Events is {drought_perc}, for Pluvial Events is {pluvial_perc}, and they were calculated at {datetime.now()}')
