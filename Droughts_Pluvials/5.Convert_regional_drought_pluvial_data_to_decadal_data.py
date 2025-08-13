6aefa2e1033c2ab030728c3209e4f1617b0174a4
Bryony Louise <bryonylouise2@hotmail.com>
2025-08-13 11:38:05 -0500

    Create 5.Convert_regional_drought_pluvial_data_to_decadal_data.py

---
+++ Droughts_Pluvials/5.Convert_regional_drought_pluvial_data_to_decadal_data.py
@@ -0,0 +1,60 @@
+#########################################################################################
+## Identify drought and pluvial occurrences for all grid points across the CONUS between 1915-2020.
+## Bryony Louise
+## Last Edited: Wednesday, August 13th, 2025
+#########################################################################################
+#Import Required Modules
+#########################################################################################
+import xesmf as xe
+import numpy as np
+import xarray as xr
+import dask
+from tqdm import tqdm
+import time
+from datetime import datetime, timedelta, date
+from netCDF4 import Dataset, num2date, MFDataset
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import spei as si
+import pandas as pd
+import scipy.stats as scs
+import os
+import gzip
+
+#########################################################################################
+#Import Functions
+#########################################################################################
+import functions
+
+#########################################################################################
+#Convert regional files into decadal time periods 
+#########################################################################################
+Regions = {"WCN", "WCS", "MWN", "MWC", "MWS", "NGP", "SGP", "NGL", "SGL", "NNE", "SNE", "ESE", "WSE"}
+time_periods = {'1915_1924','1925_1934','1935_1944','1945_1954', '1955_1964','1965_1974','1975_1984','1985_1994','1995_2004','2005_2014','2015_2020'}
+
+dirname= '/data2/bpuxley/droughts_and_pluvials/regional_data/'
+
+pathfiles = []
+for i in Regions:
+        filename = 'droughts_and_pluvials_%s.nc'%(i)
+        pathfile = os.path.join(dirname, filename)
+        pathfiles.append(pathfile)
+
+datasets = [xr.open_dataset(f) for f in pathfiles]
+print('Read in Data')
+
+#Split data in 10 year time periods
+for period in sorted(time_periods):
+	print(period)
+	data = [ds.sel(time=slice(period[:4]+"-01-01",period[5:]+"-12-31")) for ds in datasets]
+	
+	print(period+': Started')
+	combined = xr.combine_by_coords(data)
+	print(period+': Ended')
+	
+	print('Saving '+period+': Started')
+	combined.to_netcdf('/data2/bpuxley/droughts_and_pluvials/droughts_and_pluvials_%s.nc'%(period))
+	print('Saving '+period+': Ended')
+
+	del(combined)
