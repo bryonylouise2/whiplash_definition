65e59cbb9c4abf627637e39d764277a826a805f4
Bryony Louise <bryonylouise2@hotmail.com>
2025-09-17 15:54:22 -0500

    Update 3.Convert_regional_spi_files_to_decadal_files.py

--- 3.Convert_regional_spi_files_to_decadal_files.py
+++ 3.Convert_regional_spi_files_to_decadal_files.py
@@ -11,22 +11,8 @@
 #########################################################################################
 # Import Required Modules
 #########################################################################################
-import xesmf as xe
-import numpy as np
-import xarray as xr
-import dask
-from tqdm import tqdm
-import time
-from datetime import datetime, timedelta, date
-from netCDF4 import Dataset, num2date, MFDataset
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import spei as si
-import pandas as pd
-import scipy.stats as scs
 import os
-import gzip
+import xarray as xr
 
 #########################################################################################
 # Convert regional files into decadal periods 
